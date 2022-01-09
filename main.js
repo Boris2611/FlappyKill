3d0598038b9be73d0446a7bd1ae4a02a6a994e2f
Boris2611 <84404796+Boris2611@users.noreply.github.com>
2022-01-09 21:20:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const smashSound = new Audio("smash.wav");
+const smashpipe = new Audio("smashpipe.wav");
+const splashSound = new Audio("splash.wav");
 
 let pipeBack = 1;
 let pipeOffset;
@@ -9,9 +11,10 @@ let score = 0;
 setInterval(offsetCheck, 10); // Geting offset
 
 const birdKilled = () => {
+    smashSound.play(); // Sound when bird is killed
     setTimeout(function(){killed=0;}, 500);
     score += 1;
-    document.getElementById("score").innerHTML = "Score: " + score;
+    document.getElementById("score").innerHTML = ": " + score;
     document.getElementById("blood").style.visibility = "visible";
 
     if (score == 2) {
@@ -24,6 +27,8 @@ const birdKilled = () => {
 
     if (score == 10) {
         document.getElementById("birdbox").style.animation = "birdMovementFast 1.5s linear infinite";
+        document.getElementById("bloodSplash").style.visibility = "visible";
+        splashSound.play();
     }
 
     if (score == 14) {
@@ -32,13 +37,15 @@ const birdKilled = () => {
 
     if (score == 20) {
         document.getElementById("birdbox").style.animation = "birdMovementFast 1.2s linear infinite";
+        document.getElementById("bloodSplash2").style.visibility = "visible";
+        splashSound.play();
     }
 };
 
 const smash = () => {
+    smashpipe.play();
     if (pipeBack == 1) {
         pipeBack = 0;
-        smashSound.play(); // Sound when bird is killed
         document.getElementById("pipe1").style.top = "0px";
         setTimeout(function() {document.getElementById("pipe1").style.top = "-200px"; pipeBack=1}, 500)
 
@@ -60,6 +67,8 @@ function checkLose() {
         killed = 1;
         document.getElementById("blood").style.visibility = "hidden";
         document.getElementById("birdbox").style.animation = "birdMovement 2s linear infinite";
+        document.getElementById("bloodSplash").style.visibility = "hidden";
+        document.getElementById("bloodSplash2").style.visibility = "hidden";
     }
 }
     
